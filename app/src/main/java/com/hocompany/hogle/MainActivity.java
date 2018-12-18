b18a894887d29412a25988516ec2180a440c3af0
hokyung <ghrud92@gmail.com>
2018-12-18 17:37:38 +0900

    한글 값 테스트 관련 코드 추가

--- app/src/main/java/com/hocompany/hogle/MainActivity.java
+++ app/src/main/java/com/hocompany/hogle/MainActivity.java
@@ -10,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText) findViewById(R.id.test_input);
+        final int BASE_CODE = 0xAC00;   // '가' 44032
+        final int BASE_INIT = 0x1100;   // 'ㄱ' 4352
+        final int BASE_VOWEL = 0x1161;  // 'ㅏ' 4449
+
+        char a = (char) BASE_INIT;
+        char b = (char) BASE_VOWEL;
+        String test = (int) a + " " + (int) b;
+        editText.setText(test);
     }
 }
